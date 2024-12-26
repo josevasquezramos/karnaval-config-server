ef7c009821ebb5ff5e8da5acfd018994d9703a58
Jose Vasquez <jmvr16092003@gmail.com>
2024-12-26 03:33:49 -0500

    upload

---
+++ karnaval-discovery-server/src/main/java/com/sistemas/karnaval/KarnavalDiscoveryServerApplication.java
@@ -0,0 +1,15 @@
+package com.sistemas.karnaval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class KarnavalDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarnavalDiscoveryServerApplication.class, args);
+	}
+
+}
